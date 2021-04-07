2fd0ba4317ebe1e6ea1c47df6f2fd4363a0897f7
LHJE <theeluke@gmail.com>
2021-04-07 14:38:30 -0600

    Update post model spec with tests

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Validations' do
+    it {should validate_presence_of :content}
+    it {should allow_value(false).for(:grass_tags)}
+    it {should allow_value(false).for(:only_followers)}
+  end
 end
