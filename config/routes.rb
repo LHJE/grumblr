31989933a91aa88c0d5c189c2dce3ad427f31f65
LHJE <theeluke@gmail.com>
2021-04-07 14:11:17 -0600

    Update routes to have login, logout, and register routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,10 @@ Rails.application.routes.draw do
   root to: 'posts#index'
   resources :posts
   resources :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
+  get '/registration', to: 'users#new', as: :registration
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#login'
+  get '/logout', to: 'sessions#logout'
+
 end
