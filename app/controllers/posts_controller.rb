864cf6dfe69d0ff91f2a427adb2bda59822c9aa2
LHJE <theeluke@gmail.com>
2021-04-07 15:15:48 -0600

    Update posts controller to allow `:only_followers` to be in params

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -22,7 +22,6 @@ class PostsController < ApplicationController
   # POST /posts or /posts.json
   def create
     @post = Post.new(post_params)
-
     respond_to do |format|
       if @post.save
         format.html { redirect_to @post, notice: "Post was successfully created." }
@@ -64,6 +63,6 @@ class PostsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def post_params
-      params.require(:post).permit(:content, :grass_tags)
+      params.require(:post).permit(:content, :grass_tags, :only_followers)
     end
 end
