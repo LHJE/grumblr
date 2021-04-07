01192127ce2119c4a1aa571afde538f91faf6aa1
LHJE <theeluke@gmail.com>
2021-04-07 14:50:02 -0600

    Update user show view spec to test for name, email, and correct links

--- spec/views/users/show.html.erb_spec.rb
+++ spec/views/users/show.html.erb_spec.rb
@@ -1,18 +1,44 @@
 require 'rails_helper'
 
-RSpec.describe "users/show", type: :view do
-  before(:each) do
-    @user = assign(:user, User.create!(
-      name: "Name",
-      email: "Email",
-      password_digest: "Password Digest"
-    ))
-  end
+RSpec.describe 'User show', type: :feature do
+  describe 'As a Visitor' do
+    describe 'I am sent to user show page ' do
+      before :each do
+        @user = User.create(name: 'Jackie Chan', email: 'a@a.com', password: 'a', password_confirmation: 'a')
+        @post = Post.create(
+          content: "MyText",
+          grass_tags: "Grass Tags",
+          only_followers: true
+        )
 
-  it "renders attributes in <p>" do
-    render
-    expect(rendered).to match(/Name/)
-    expect(rendered).to match(/Email/)
-    expect(rendered).to match(/Password Digest/)
+        UserPost.create(user_id: @user.id, post_id: @post.id)
+      end
+
+      it "visits user page" do
+        visit user_path(@user)
+
+        expect(page).to have_content('Name')
+        expect(page).to have_content('Jackie Chan')
+        expect(page).to have_content('Email')
+        expect(page).to have_content('a@a.com')
+        expect(page).to_not have_link('Edit')
+        expect(page).to_not have_link('Destroy')
+        expect(page).to have_link('Back')
+      end
+
+      it 'clicks on name on main page' do
+        allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
+
+        visit user_path(@user)
+
+        expect(page).to have_content('Name')
+        expect(page).to have_content('Jackie Chan')
+        expect(page).to have_content('Email')
+        expect(page).to have_content('a@a.com')
+        expect(page).to have_link('Edit')
+        expect(page).to have_link('Destroy')
+        expect(page).to have_link('Back')
+      end
+    end
   end
 end
