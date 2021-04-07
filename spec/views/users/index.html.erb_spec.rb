5f93afab5bf65081721ff6df5aab2587642aae07
LHJE <theeluke@gmail.com>
2021-04-07 14:09:16 -0600

    Update user index spec to expect to see names of users

--- spec/views/users/index.html.erb_spec.rb
+++ spec/views/users/index.html.erb_spec.rb
@@ -1,25 +1,24 @@
 require 'rails_helper'
 
-RSpec.describe "users/index", type: :view do
+RSpec.describe "users/index", type: :feature do
   before(:each) do
-    assign(:users, [
-      User.create!(
-        name: "Name",
-        email: "Email",
-        password_digest: "Password Digest"
-      ),
-      User.create!(
-        name: "Name",
-        email: "Email",
-        password_digest: "Password Digest"
-      )
-    ])
+    User.create!(
+      name: "Jackie",
+      email: "Email",
+      password: 'a',
+      password_confirmation: 'a'
+    )
+    User.create!(
+      name: "Dane",
+      email: "Email2",
+      password: 'a',
+      password_confirmation: 'a'
+    )
   end
 
   it "renders a list of users" do
-    render
-    assert_select "tr>td", text: "Name".to_s, count: 2
-    assert_select "tr>td", text: "Email".to_s, count: 2
-    assert_select "tr>td", text: "Password Digest".to_s, count: 2
+    visit users_path
+    expect(page).to have_content('Jackie')
+    expect(page).to have_content('Dane')
   end
 end
