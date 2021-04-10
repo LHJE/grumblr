0a54097835b727873d03133d45d5d8d476625eb7
LHJE <theeluke@gmail.com>
2021-04-10 13:10:38 -0600

    Update routes with user/dashboard route

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,13 @@ Rails.application.routes.draw do
   root to: 'posts#index'
   resources :posts
   resources :users
-  
+
   get '/registration', to: 'users#new', as: :registration
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#login'
   get '/logout', to: 'sessions#logout'
 
+  namespace :user do
+    get '/dashboard', to: 'dashboard#show'
+  end
 end
