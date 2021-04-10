d50e2028e243ea4c2daeefff39ffaf5ff154a3c3
LHJE <theeluke@gmail.com>
2021-04-10 12:25:49 -0600

    Update post_controller with new index method, only showing posts ... the user can see, including ones they can only see if the follow another user.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,10 +4,25 @@ class PostsController < ApplicationController
   # GET /posts or /posts.json
   def index
     if current_user
-      require "pry"; binding.pry
+      if !Post.where(only_followers: true)[0].nil? && !FollowerFollowed.where(follower_id: current_user.id)[0].nil?
+# This one is a doosie.  It sorta starts in the middle and spirals out.  What we're trying to find here is all the public non-"Only Followers" posts as well as the posts that this user can see because they follow a user who has "Only Followers" posts.
+
+# So first we find the FollowerFollowed models that have the current_user's id in the follower_id column.
+# Then we pluck all the `followed_id`'s out, since those are the id's of the users this current_user is following.
+# Then we find all the UserPost models that have those `followed_id`'s.
+# Then we pluck all the `post_id`'s out of the UserPosts that we now have.  These are the ids of posts that the user the current_user follows has posted (what a sentence!)
+# These ids are used to find Posts, but only posts with the matching id's that ALSO have `only_followers`set to true.
+# Before ordering anything, we want to make sure that we still have all the public posts, so there's an `.or()` at the end getting all public ones.
+# Lastly, we order them in descending order so the newest ones come up first.
+
+        @posts = Post.where(id: UserPost.where(user_id: FollowerFollowed.where(follower_id: current_user.id).pluck(:followed_id)).pluck(:post_id), only_followers: true).or(Post.where(only_followers: false)).order(id: :desc)
+      else
+        @posts = Post.where(only_followers: false).order(id: :desc)
+      end
     else
-      @posts = Post.where.not(only_followers: true)
+      @posts = Post.where(only_followers: false).order(id: :desc)
     end
+
   end
 
   # GET /posts/1 or /posts/1.json
