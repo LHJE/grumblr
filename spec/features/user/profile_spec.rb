8fe099080724a1a4f97e09619cc2dc529e4095bb
LHJE <theeluke@gmail.com>
2021-04-11 14:19:41 -0600

    Add `profile_spec` with test for non-logged in user

---
+++ spec/features/user/profile_spec.rb
@@ -0,0 +1,53 @@
+require 'rails_helper'
+include ActionView::Helpers::NumberHelper
+
+RSpec.describe 'Profile Page' do
+  describe 'As an authenticated  user' do
+    before :each do
+      @user_1 = User.create(name: 'Jackie Chan', email: '67@67.com', password: '67', password_confirmation: '67')
+      @user_2 = User.create!(name: 'Cynthia Rothrock', email: 'b@b.com', password: 'a', password_confirmation: 'a')
+      @user_3 = User.create!(name: 'Michelle Yeoh', email: 'c@c.com', password: 'a', password_confirmation: 'a')
+      @user_4 = User.create!(name: 'Scott Adkins', email: 'd@d.com', password: 'a', password_confirmation: 'a')
+      FollowerFollowed.create!(follower_id: @user_1.id, followed_id: @user_2.id)
+      @post_1 = Post.create!(
+        content: "This post",
+        only_followers: false,
+        user_id: @user_1.id
+      )
+      @post_2 = Post.create!(
+        content: "The other post",
+        grass_tags: "other",
+        only_followers: true,
+        user_id: @user_1.id
+      )
+      @post_3 = Post.create!(
+        content: "The hidden post",
+        grass_tags: "hidden",
+        only_followers: true,
+        user_id: @user_2.id
+      )
+      @post_4 = Post.create!(
+        content: "The hidden other post",
+        grass_tags: "hidden, other",
+        only_followers: true,
+        user_id: @user_3.id
+      )
+    end
+
+    it "If not logged in, I can see my Grumbls if I have Grumbls, but not the only_followers ones" do
+
+      visit "users/#{@user_1.id}"
+
+      expect(page).to have_content("Jackie Chan")
+      expect(page).to have_content("Jackie Chan's Grumbls:")
+      expect(page).to have_content(@post_1.content)
+      expect(page).to_not have_content(@post_2.content)
+      expect(page).to_not have_content(@post_2.grass_tags)
+      expect(page).to_not have_content(@post_3.content)
+      expect(page).to_not have_content(@post_3.grass_tags)
+      expect(page).to_not have_content(@post_4.content)
+      expect(page).to_not have_content(@post_4.grass_tags)
+    end
+
+  end
+end
