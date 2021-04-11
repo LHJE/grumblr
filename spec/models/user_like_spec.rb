5e5adeca1d8b94e89410e272a18ed037a1b03234
LHJE <theeluke@gmail.com>
2021-04-10 18:46:14 -0600

    Update model spec for UserLiked to UserLike for clarity's sake

--- spec/models/user_like_spec.rb
+++ spec/models/user_like_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe UserLiked, type: :model do
+RSpec.describe UserLike, type: :model do
   describe 'Validations' do
     it {should validate_presence_of :user_id}
     it {should validate_presence_of :post_id}
