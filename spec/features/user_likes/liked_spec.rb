504b9027df099921e84230ad39d235c45e67b4cc
LHJE <theeluke@gmail.com>
2021-04-11 17:25:34 -0600

    Update liked spec with more precise tests

--- spec/features/user_likes/liked_spec.rb
+++ spec/features/user_likes/liked_spec.rb
@@ -14,7 +14,7 @@ RSpec.describe 'UserLiked' do
       content: "The other post",
       grass_tags: "other",
       only_followers: false,
-      user_id: @user_1.id
+      user_id: @user_2.id
     )
     @post_3 = Post.create!(
       content: "The hidden post",
@@ -56,36 +56,23 @@ RSpec.describe 'UserLiked' do
   describe 'As a User' do
     before :each do
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user_1)
+      visit root_path
     end
 
     it "I can like a grumbl from the index" do
-      expect(page).to have_content(@post_1.content)
-      expect(page).to have_content(@post_2.content)
-      expect(page).to have_content(@post_2.grass_tags)
-      expect(page).to_not have_content(@post_3.content)
-      expect(page).to_not have_content(@post_3.grass_tags)
-      expect(page).to_not have_content(@post_4.content)
-      expect(page).to_not have_content(@post_4.grass_tags)
 
-      within(".post-#{@post_1.id}") do
+      within(".post-#{@post_3.id}") do
         click_link("Like")
       end
 
       expect(page).to have_content("You have liked the grumbl!")
 
-      within(".post-#{@post_1.id}") do
+      within(".post-#{@post_3.id}") do
         expect(page).to have_content("1 Likes")
       end
     end
 
     it "I can unlike a grumbl from the index" do
-      expect(page).to have_content(@post_1.content)
-      expect(page).to have_content(@post_2.content)
-      expect(page).to have_content(@post_2.grass_tags)
-      expect(page).to_not have_content(@post_3.content)
-      expect(page).to_not have_content(@post_3.grass_tags)
-      expect(page).to_not have_content(@post_4.content)
-      expect(page).to_not have_content(@post_4.grass_tags)
 
       within(".post-#{@post_2.id}") do
         click_link("Unlike")
@@ -96,6 +83,26 @@ RSpec.describe 'UserLiked' do
       within(".post-#{@post_2.id}") do
         expect(page).to have_content("0 Likes")
       end
+    end
+
+    it "I can like a grumbl from the show page" do
+      visit "posts/#{@post_3.id}"
+
+      click_link("Like")
+
+      expect(page).to have_content("You have liked the grumbl!")
+
+      expect(page).to have_content("Likes: 1")
+    end
+
+    it "I can unlike a grumbl from the show page" do
+      visit "posts/#{@post_2.id}"
+
+      click_link("Unlike")
+
+      expect(page).to have_content("You have unliked the grumbl!")
+
+      expect(page).to have_content("Likes: 0")
     end
   end
 end
