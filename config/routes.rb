cda512c7a44b0a66cb81c394f5f5fcb354d716b5
LHJE <theeluke@gmail.com>
2021-04-11 17:28:47 -0600

    Update routes with `user_likes` routes

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,9 @@ Rails.application.routes.draw do
   get '/follower_followed/:id/create', to: 'follower_followed#create'
   get '/follower_followed/:id/destroy', to: 'follower_followed#destroy'
 
+  get '/user_likes/:id/create', to: 'user_likes#create'
+  get '/user_likes/:id/destroy', to: 'user_likes#destroy'
+
   namespace :user do
     get '/dashboard', to: 'dashboard#show'
   end
