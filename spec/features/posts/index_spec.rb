f5ef5eb0033a1bc78fa4485bc8295f0402ccb515
LHJE <theeluke@gmail.com>
2021-04-12 12:00:55 -0600

    Update post index spec with typo fix

--- spec/features/posts/index_spec.rb
+++ spec/features/posts/index_spec.rb
@@ -36,7 +36,7 @@ RSpec.describe 'Post Index' do
 
       expect(current_path).to eq(root_path)
       expect(page).to have_content("GRUMBLR")
-      expect(page).to have_content("Where All Your Minor Inconviencies Can Be Shared")
+      expect(page).to have_content("Where All Your Minor Inconveniences Can Be Shared")
       expect(page).to have_content("i'm sure nothing will go wrong with this site")
       expect(page).to have_content("Here's What's Grumblin:")
       expect(page).to have_content(@post_1.content)
