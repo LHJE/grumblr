25c5acffa429f6a3da910e5b958cfbf4625fa93b
LHJE <theeluke@gmail.com>
2021-04-12 11:33:08 -0600

    Update `user/liked_controller` to allow for `@user` object

--- app/controllers/user/liked_controller.rb
+++ app/controllers/user/liked_controller.rb
@@ -3,6 +3,7 @@ class User::LikedController < ApplicationController
   def show
 
     @posts = Post.where(id: UserLike.where(user_id: params[:id]).pluck(:post_id)).order(id: :desc)
+    @user = User.where(id: params[:id])[0]
     @users = User.where(id: @posts.pluck(:user_id)) if @posts != []
     @likes = UserLike.where(post_id: @posts.pluck(:id)) if @posts != []
   end
