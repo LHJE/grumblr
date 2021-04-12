70a996a33f173f0e9de9bf7ad1c3be0542c5d6e9
LHJE <theeluke@gmail.com>
2021-04-12 11:43:43 -0600

    Update routes with `posts/new` post route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root to: 'posts#index'
   resources :posts
+  post '/posts/new', to: 'posts#new'
+
   resources :users
   post '/users/:id/edit', to: 'users#edit'
 
