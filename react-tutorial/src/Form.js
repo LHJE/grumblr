c6918256ed74d3013c3f926b14136b94b55e8e5c
Luke Hunter James-Erickson <theeluke@gmail.com>
2021-06-28 15:07:16 -0600

    Update `Form.js` to be able to handle submit button

--- react-tutorial/src/Form.js
+++ react-tutorial/src/Form.js
@@ -15,26 +15,32 @@ class Form extends Component {
     job: '',
   }
 
+  submitForm = () => {
+    this.props.handleSubmit(this.state)
+    this.setState(this.initialState)
+  }
+
   state = this.initialState
   render() {
     const { name, job } = this.state;
 
     return (
       <form>
-      <label htmlFor="name">Name</label>
-      <input
-      type="text"
-      name="name"
-      id="name"
-      value={name}
-      onChange={this.handleChange} />
-      <label htmlFor="job">Job</label>
-      <input
-      type="text"
-      name="job"
-      id="job"
-      value={job}
-      onChange={this.handleChange} />
+        <label htmlFor="name">Name</label>
+        <input
+        type="text"
+        name="name"
+        id="name"
+        value={name}
+        onChange={this.handleChange} />
+        <label htmlFor="job">Job</label>
+        <input
+        type="text"
+        name="job"
+        id="job"
+        value={job}
+        onChange={this.handleChange} />
+        <input type="button" value="Submit" onClick={this.submitForm} />
       </form>
     );
   }
