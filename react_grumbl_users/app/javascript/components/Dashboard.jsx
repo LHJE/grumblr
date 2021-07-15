0b9aff6780fe283676133d315fc41b0058e59e2b
Luke Hunter James-Erickson <theeluke@gmail.com>
2021-07-15 11:55:58 -0600

    Resetting `react_grumbl_users` to try to get it to show anything at all

--- react_grumbl_users/app/javascript/components/Dashboard.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { Link } from "react-router-dom";
-
-export default function Dashboard() {
-  return(
-    <h2>Dashboard</h2>
-  );
-}
--- react_grumbl_users/app/javascript/routes/Index.jsx
+++ react_grumbl_users/app/javascript/routes/Index.jsx
@@ -4,8 +4,6 @@ import Home from "../components/Home";
 import Grumbls from "../components/Grumbls";
 import Grumbl from "../components/Grumbl";
 import NewGrumbl from "../components/NewGrumbl";
-import Dashboard from '../Dashboard/Dashboard';
-import Preferences from '../Preferences/Preferences';
 
 export default (
   <Router>
@@ -14,8 +12,6 @@ export default (
       <Route path="/grumbls" exact component={Grumbls} />
       <Route path="/grumbl/:id" exact component={Grumbl} />
       <Route path="/grumbl" exact component={NewGrumbl} />
-      <Route path="/dashboard" exact component={Dashboard} />
-      <Route path="/preferences" exact component={Preferences} />
     </Switch>
   </Router>
 );
