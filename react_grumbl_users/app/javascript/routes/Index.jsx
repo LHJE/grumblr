4afbbaa0cda732fb1da4e78a1d26030937d2d99d
Luke Hunter James-Erickson <theeluke@gmail.com>
2021-07-15 11:29:01 -0600

    Update `react_grumbl_users` remixing tutorial

--- react_grumbl_users/app/javascript/routes/Index.jsx
+++ react_grumbl_users/app/javascript/routes/Index.jsx
@@ -4,6 +4,8 @@ import Home from "../components/Home";
 import Grumbls from "../components/Grumbls";
 import Grumbl from "../components/Grumbl";
 import NewGrumbl from "../components/NewGrumbl";
+import Dashboard from '../Dashboard/Dashboard';
+import Preferences from '../Preferences/Preferences';
 
 export default (
   <Router>
@@ -12,6 +14,8 @@ export default (
       <Route path="/grumbls" exact component={Grumbls} />
       <Route path="/grumbl/:id" exact component={Grumbl} />
       <Route path="/grumbl" exact component={NewGrumbl} />
+      <Route path="/dashboard" exact component={Dashboard} />
+      <Route path="/preferences" exact component={Preferences} />
     </Switch>
   </Router>
 );
