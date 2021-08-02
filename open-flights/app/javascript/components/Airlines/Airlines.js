16d5315dcdd2660d1081c3ef0bac07debff5b35d
Luke Hunter James-Erickson <theeluke@gmail.com>
2021-08-02 09:28:16 -0600

    Add Airlines.js via tutorial

---
+++ open-flights/app/javascript/components/Airlines/Airlines.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Airlines = () => {
+	const [airlines, setAirlines] = useState([])
+
+	useEffect(() => {
+		
+	})
+
+	return (
+		<div> This is the airlines index!!! </div>
+	)
+}
+
+export default Airlines
