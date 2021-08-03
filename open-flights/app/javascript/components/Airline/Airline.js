319aab97b7ca809219977a9e2cc00ba93d45d38a
Luke Hunter James-Erickson <theeluke@gmail.com>
2021-08-03 11:32:04 -0600

    Update `Airline.js`/Airline show page to show actual airline data and... review data

--- open-flights/app/javascript/components/Airline/Airline.js
+++ open-flights/app/javascript/components/Airline/Airline.js
@@ -1,7 +1,69 @@
-import React from 'react'
+import React, { useState, useEffect, Fragment } from 'react'
+import axios from 'axios'
+import Header from './Header'
+import ReviewForm from './ReviewForm'
+import styled from 'styled-components'
 
-const Airline = () => {
-	return <div> This is the airlines show view!!! </div>
+const Wrapper = styled.div`
+	margin-left: auto;
+	margin-right: auto;
+	display: grid;
+	grid-template-columns: repeat(2, 1fr);
+`
+
+const Column = styled.div`
+	background: #fff;
+	height: 100vh;
+	overflow: scroll;
+
+	&:last-child {
+		background: #000;
+	}
+`
+
+const Main = styled.div`
+	padding-left: 50px;
+`
+
+const Airline = (props) => {
+	const [airline, setAirline] = useState({})
+	const [review, setReview] = useState({})
+	const [loaded, setLoaded] = useState(false)
+
+	useEffect(()=>{
+		const slug = props.match.params.slug
+		const url = `/api/v1/airlines/${slug}`
+
+		axios.get(url)
+		.then( resp => {
+			setAirline(resp.data)
+			setLoaded(true)
+		})
+		.catch( resp => console.log(resp) )
+	}, [])
+
+	return (
+		<Wrapper>
+			{
+				loaded &&
+				<Fragment>
+					<Column>
+						<Main>
+								<Header
+									attributes={airline.data.attributes}
+									reviews={airline.included}
+								/>
+							<div className="reviews"></div>
+						</Main>
+					</Column>
+					<Column>
+						<ReviewForm/>
+
+					</Column>
+				</Fragment>
+			}
+		</Wrapper>
+	)
 }
 
 export default Airline
