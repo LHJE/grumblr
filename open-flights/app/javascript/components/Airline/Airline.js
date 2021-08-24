d117c4116970635364f37fa989ad71919516e014
Luke Hunter James-Erickson <theeluke@gmail.com>
2021-08-24 14:59:29 -0600

    Update regular OpenFlights app based on differences between it and ... OpenFlightsfinished

--- open-flights/app/javascript/components/Airline/Airline.js
+++ open-flights/app/javascript/components/Airline/Airline.js
@@ -2,7 +2,9 @@ import React, { useState, useEffect, Fragment } from 'react'
 import axios from 'axios'
 import Header from './Header'
 import ReviewForm from './ReviewForm'
+import Review from './Review'
 import styled from 'styled-components'
+
 
 const Wrapper = styled.div`
 	margin-left: auto;
@@ -42,6 +44,45 @@ const Airline = (props) => {
 		.catch( resp => console.log(resp) )
 	}, [])
 
+	const handleChange = (e) => {
+		e.preventDefault()
+
+		setReview(Object.assign({}, review, {[e.target.name]: e.target.value}))
+	}
+
+	const handleSubmit = (e) => {
+		e.preventDefault()
+
+		const csrfToken = document.querySelector('[name=csrf-token]').content
+		axios.defaults.headers.common['X-CSRF-TOKEN'] = csrfToken
+
+		const airline_id = airline.data.id
+		axios.post('/api/v1/reviews', {review, airline_id})
+		.then( resp => {
+			const included = [...airline.included, resp.data.data]
+			setAirline({...airline, included})
+			setReview({title: '', description: '', score: 0})
+		})
+		.catch( resp => {console.log(resp)})
+	}
+
+	const setRating = (score, e) => {
+		e.preventDefault()
+		setReview({...review, score})
+	}
+
+	let reviews
+	if (loaded && airline.included) {
+		reviews = airline.included.map( (item, index) => {
+			return (
+				<Review
+					key={index}
+					attributes={item.attributes}
+				/>
+			)
+		})
+	}
+
 	return (
 		<Wrapper>
 			{
@@ -53,12 +94,17 @@ const Airline = (props) => {
 									attributes={airline.data.attributes}
 									reviews={airline.included}
 								/>
-							<div className="reviews"></div>
+							{reviews}
 						</Main>
 					</Column>
 					<Column>
-						<ReviewForm/>
-
+						<ReviewForm
+							handleChange={handleChange}
+							handleSubmit={handleSubmit}
+							setRating={setRating}
+							attributes={airline.data.attributes}
+							review={review}
+						/>
 					</Column>
 				</Fragment>
 			}
