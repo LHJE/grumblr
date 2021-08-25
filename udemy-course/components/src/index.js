e74f6dc1aeab267100393cf4553297949224e059
Luke Hunter James-Erickson <theeluke@gmail.com>
2021-08-25 11:06:49 -0600

    Update index.js with futher prop updates

--- udemy-course/components/src/index.js
+++ udemy-course/components/src/index.js
@@ -4,13 +4,28 @@ import faker from 'faker';
 import CommentDetail from './CommentDetail';
 
 const App = () => {
-	return (
-      <div className="ui container comments">
-        <CommentDetail author="Sam" />
-        <CommentDetail author="Alex" />
-        <CommentDetail author="Jane" />
-      </div>
+  return (
+    <div className="ui container comments">
+      <CommentDetail
+        author="Sam"
+        timeAgo="Today at 6:00PM"
+        content="It's a post!"
+        image={faker.image.avatar()} />
+      <CommentDetail
+        author="Alex"
+        timeAgo="Today at 2:00AM"
+        content="It's another post!"
+        image={faker.image.avatar()} />
+      <CommentDetail
+        author="Jane"
+        timeAgo="Yesterday at 5:00PM"
+        content="It's a third post!"
+        image={faker.image.avatar()} />
+    </div>
   );
 };
 
-ReactDOM.render(<App />, document.querySelector('#root'));
+ReactDOM.render(
+  <App />,
+  document.querySelector('#root')
+);
