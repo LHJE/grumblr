d3000474faf81687f0a4b61d387b1ab4575be48d
Luke Hunter James-Erickson <theeluke@gmail.com>
2021-08-25 10:24:11 -0600

    Update index.js to add Faker and put in CommentDetail components

--- udemy-course/components/src/index.js
+++ udemy-course/components/src/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import faker from 'faker';
+import CommentDetail from './CommentDetail';
 
 const App = () => {
-	return <div>Hi there!</div>
+	return (
+      <div className="ui container comments">
+        <CommentDetail author="Sam" />
+        <CommentDetail author="Alex" />
+        <CommentDetail author="Jane" />
+      </div>
+  );
 };
 
 ReactDOM.render(<App />, document.querySelector('#root'));
