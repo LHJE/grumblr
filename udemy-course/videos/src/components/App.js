faef0e85c17de147e0ec8666a70d59b16ec8f4b4
Luke Hunter James-Erickson <theeluke@gmail.com>
2022-01-07 13:50:51 -0700

    Add SearchBar class

--- udemy-course/videos/src/components/App.js
+++ udemy-course/videos/src/components/App.js
@@ -1,8 +1,17 @@
 import React from 'react';
+import SearchBar from './SearchBar'
 
 class App extends React.Component {
+  onTermSubmit = (term) => {
+
+  };
+
   render() {
-    return <div>App</div>;
+    return (
+      <div className='ui container'>
+        <SearchBar onFormSubmit={this.onTermSubmit} />
+      </div>
+    );
   }
 }
 
